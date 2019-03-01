b6a5e5cfc87624daba3fd5337c9e4ed8be97cea1
jesper <brodinjesper@gmail.com>
2019-03-01 17:03:36 +0100

    First draft of categoryPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CategoryPageComponent } from './category-page/category-page.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryPageComponent
+    CategoryPageComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/category-page/category-page.component.spec.ts
+++ src/app/category-page/category-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {MatButtonModule} from '@angular/material/button';
 import { CategoryPageComponent } from './category-page.component';
 
 describe('CategoryPageComponent', () => {
