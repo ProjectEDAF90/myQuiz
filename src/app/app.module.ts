a20884d0e91838ad8b17157ae22ca1443f906ddb
jesper <brodinjesper@gmail.com>
2019-03-01 14:04:47 +0100

    added component categoryPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CategoryPageComponent } from './category-page/category-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryPageComponent
   ],
   imports: [
     BrowserModule,
