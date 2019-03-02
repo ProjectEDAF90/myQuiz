7593055d9f263d86eb8f63c5d6599acfa194ad0f
Edvin <edvin@boyner.se>
2019-03-02 15:05:11 +0100

    Första draften på frontpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FrontPageComponent } from './front-page/front-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FrontPageComponent } from "./front-page/front-page.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FrontPageComponent
-  ],
+  declarations: [AppComponent, FrontPageComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { User } from "../user";
 
 @Component({
-  selector: 'app-front-page',
-  templateUrl: './front-page.component.html',
-  styleUrls: ['./front-page.component.css']
+  selector: "app-front-page",
+  templateUrl: "./front-page.component.html",
+  styleUrls: ["./front-page.component.css"]
 })
 export class FrontPageComponent implements OnInit {
+  user: User = {
+    id: 1,
+    name: ""
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
+
+// import { Component, OnInit } from "@angular/core";
+// import { FormGroup, FormControl } from "@angular/forms";
+
+// @Component({
+//   selector: "app-front-page",
+//   templateUrl: "./front-page.component.html",
+//   styleUrls: ["./front-page.component.css"]
+// })
+// export class FrontPageComponent implements OnInit {
+//   nickNameForm: FormGroup;
+//   constructor() {}
+
+//   ngOnInit() {
+//     this.nickNameForm = new FormGroup({
+//       fullName: new FormControl()
+//     });
+//   }
+// }
