9c66f754f16c67b73b7bfb61f71d388082356040
jesper <brodinjesper@gmail.com>
2019-03-02 16:20:53 +0100

    Simple routing test to quizpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { QuizPageComponent } from './quiz-page/quiz-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categories', component: CategoryPageComponent},
+  {path: 'quiz', component: QuizPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
