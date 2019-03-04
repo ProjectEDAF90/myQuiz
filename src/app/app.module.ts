e8f6b73b24bccc3c5620defcf368dd722b6280ee
jesper <brodinjesper@gmail.com>
2019-03-04 13:35:43 +0100

    From master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -17,7 +17,8 @@ import { QuizPageComponent } from './quiz-page/quiz-page.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
