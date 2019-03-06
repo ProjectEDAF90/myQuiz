92edb30b256fc9950740b56f4a78e66d48605acb
Signe Berdén <si3087be-s@student.lu.se>
2019-03-06 12:07:51 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CategoryPageComponent } from './category-page/category-page.component';
-import { QuizPageComponent } from './quiz-page/quiz-page.component';
-import {FrontPageComponent } from './front-page/front-page.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CategoryPageComponent } from "./category-page/category-page.component";
+import { QuizPageComponent } from "./quiz-page/quiz-page.component";
+import { FrontPageComponent } from "./front-page/front-page.component";
+import { ResultPageComponent } from "./result-page/result-page.component";
 
 const routes: Routes = [
-  {path:'', component: FrontPageComponent},
-  {path: 'categories', component: CategoryPageComponent},
-  {path: 'quiz', component: QuizPageComponent}
+  { path: "", component: FrontPageComponent },
+  { path: "categories", component: CategoryPageComponent },
+  { path: "quiz", component: QuizPageComponent },
+  { path: "result", component: ResultPageComponent }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/result-page/result-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
+import { ResultPageDataSource } from './result-page-datasource';
+
+@Component({
+  selector: 'app-result-page',
+  templateUrl: './result-page.component.html',
+  styleUrls: ['./result-page.component.css']
+})
+export class ResultPageComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource: ResultPageDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngOnInit() {
+    this.dataSource = new ResultPageDataSource(this.paginator, this.sort);
+  }
+}
