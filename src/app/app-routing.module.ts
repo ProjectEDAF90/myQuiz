c3855df96413364f5614ac3e135ee7c98632362e
jesper <brodinjesper@gmail.com>
2019-03-07 14:34:19 +0100

    fixed bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { CategoryPageComponent } from "./category-page/category-page.component";
 import { ResultPageComponent } from "./result-page/result-page.component";
 import { QuestionsComponent } from './questions/questions.component';
-import { StartPageComponent } from './start-page/start-page.component';
 const routes: Routes = [
-  {
-    path: '',
-    component: StartPageComponent
-  },
   {
     path: "questions",
     component: QuestionsComponent
